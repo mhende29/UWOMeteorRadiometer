3828f5c766b4b7866b287297aae1dc295f5ecda8
Denis Vida <denis.vida@gmail.com>
2018-06-07 13:12:44 +0000

    Correcting Patth Error

--- RadiometerRun.py
+++ RadiometerRun.py
@@ -16,30 +16,65 @@ from CaptureDuration import captureDuration
 from Misc import archiveDir
 
 def makeDirectory(station_code,channel,path):
-    path = "/home/pi/RadiometerData/CapturedData/"
     
+    # Get the current time
     creation_time = datetime.datetime.utcnow()
     
+    # Generate the name of the directory based on configuration information and the time of excution
     directory_name = station_code + "_" + channel + '_{:04d}{:02d}{:02d}-{:02d}{:02d}{:02d}'.format(creation_time.year,creation_time.month,creation_time.day,creation_time.hour,creation_time.minute,creation_time.second)
 
-    directory = path + directory_name
+    # Create the full path name
+    directory = os.path.join(path, directory_name)
     
+    # Create the directory to store current data 
     os.mkdir(directory,0o755) 
     
+    # Returns the path where the c code can store the data, also adds a slash at the tail of the directory as
+    # the c code requires it and double the amount of slashes as c also requires it.
     return (directory + "/").replace("/","//")
 
 ##############################################################################################################
 
 if __name__ == "__main__":
     
-    # Check to see if there is a config file
-    if (os.path.isfile('/home/pi/RadiometerData/config.txt')):
+    # Since this code is to run on a raspberry pi, define a default home for all data, and a default name
+    # for the configuration file.
+    DEFAULT_PATH = "/home/pi/RadiometerData"
+    CONFIG = "config.txt"
+    
+    
+    # Init the command line arguments parser
+    arg_parser = argparse.ArgumentParser(description=""" Starting capture and compression.""")
+    
+    # Add a mutually exclusive for the parser (the arguments in the group can't be given at the same time)
+    arg_group = arg_parser.add_mutually_exclusive_group()
+    
+    # Duration and loops are both mutually exclusive because they both provide a length of time to run for
+    arg_group.add_argument('-d', '--duration', metavar='DURATION_HOURS', help="""Start capturing right away, 
+        with the given duration in hours. """)
+    arg_group.add_argument('-l', '--loops', metavar='NUMBER_LOOPS', help="""Start capturing right away, 
+        with the given duration in loops. """)
+        
+    # check to see if a config file was provided
+    arg_parser.add_argument('-c', '--config', metavar="FILE_PATH", help="""Read the given config file as, 
+        opposed to read the file from the default path. """)
+    
+    # Parse the command line arguments
+    cml_args = arg_parser.parse_args()
+    
+    # Check if the home directory exists, if not create it and exit
+    if (not os.path.isdir(DEFAULT_PATH)):
+        os.mkdir(DEFAULT_PATH,0o755)
+        print("A new directory called RadiometerData has been created to store all data collected.\nIf no config.txt is found in this directory on the next run,\none will be generated and can then be generated and can be altered.\n\nThe program will now close.")
+        sys.exit()
+        
+    if cml_args.config:
         
         # Create a config object
         config = configparser.ConfigParser()
     
         # Read the config file into the object
-        config.read('/home/pi/RadiometerData/config.txt')
+        config.read(cml_args.config)
     
         # Gather configuration data
         station_code = config['Station']['StationCode']
@@ -48,7 +83,28 @@ if __name__ == "__main__":
         longitude = float(config['Station']['Longitude'])
         elevation = float(config['Station']['Elevation'])
         instrument_string = config['Station']['InstrumentString']
-        path = config['Station']['Path']
+        raw = config['Station']['RawData']
+        zipped = config['Station']['StoredData']
+        mode = int(config['Station']['DifferentialMode'])
+        
+        print(channel)
+        
+    # Check to see if there is a config file
+    elif (os.path.isfile(os.path.join(DEFAULT_PATH,CONFIG))):
+        
+        # Create a config object
+        config = configparser.ConfigParser()
+    
+        # Read the config file into the object
+        config.read(os.path.join(DEFAULT_PATH,CONFIG))
+    
+        # Gather configuration data
+        station_code = config['Station']['StationCode']
+        channel = config['Station']['Channel']
+        latitude = float(config['Station']['Latitude'])
+        longitude = float(config['Station']['Longitude'])
+        elevation = float(config['Station']['Elevation'])
+        instrument_string = config['Station']['InstrumentString']
         raw = config['Station']['RawData']
         zipped = config['Station']['StoredData']
         mode = int(config['Station']['DifferentialMode'])
@@ -57,7 +113,7 @@ if __name__ == "__main__":
         
         # There was no detected config file so one will be created
         # An error message explaining the issue
-        print("No config file detected in /home/pi/RadiometerData/")
+        print("No config file detected in /home/pi/RadiometerData")
         print("A default config file has been created and can be changed in RadiometerData")
         
         # Create a config object
@@ -74,33 +130,36 @@ if __name__ == "__main__":
             ('Longitude', '0.0'),
             ('Elevation', '0.0'),
             ('InstrumentString', 'Your description'),
-            ('Path','/home/pi/RadiometerData/'),
             ('RawData','CapturedData'),
-            ('StoredData','ArchivedData'),
+            ('StoredData','ZippedData'),
             ('DifferentialMode','1')
         ))
         
         # Generate the file in the desired directory and close it
-        with open('/home/pi/RadiometerData/config.txt', 'w') as configfile:config.write(configfile)
+        with open(os.path.join(DEFAULT_PATH,CONFIG), 'w') as configfile:config.write(configfile)
         configfile.closed
         
+        # Allow the user to configure the config file
+        os.chmod(os.path.join(DEFAULT_PATH,CONFIG),0o777)
+        
+        # Exit allowing the user to configure their settings
+        sys.exit()
+    
+    # Check if storage files exist, if not create them
+    # Check if both don't exist
+    if (not (os.path.isdir(os.path.join(DEFAULT_PATH,raw)) or os.path.isdir(os.path.join(DEFAULT_PATH,zipped)))):
+        os.mkdir(os.path.join(DEFAULT_PATH,raw),0o755)
+        os.mkdir(os.path.join(DEFAULT_PATH,zipped),0o755)
+        
+    # Check if the raw data folder exists and the archiving folder doesn't yet
+    elif (os.path.isdir(os.path.join(DEFAULT_PATH,raw)) and (not os.path.isdir(os.path.join(DEFAULT_PATH,zipped)))):
+        os.mkdir(os.path.join(DEFAULT_PATH,zipped),0o755)
+        
+    # Check if the archiving folder exists and the raw data folder doesn't yet
+    elif (os.path.isdir(os.path.join(DEFAULT_PATH,zipped)) and (not os.path.isdir(os.path.join(DEFAULT_PATH,raw)))):
+        os.mkdir(os.path.join(DEFAULT_PATH,raw),0o755)
+        
 ##############################################################################################################
-    
-    # Init the command line arguments parser
-    arg_parser = argparse.ArgumentParser(description=""" Starting capture and compression.
-        """)
-    
-    # Add a mutually exclusive for the parser (the arguments in the group can't be given at the same time)
-    arg_group = arg_parser.add_mutually_exclusive_group()
-    
-    arg_group.add_argument('-d', '--duration', metavar='DURATION_HOURS', help="""Start capturing right away, 
-        with the given duration in hours. """)
-    arg_group.add_argument('-l', '--loops', metavar='NUMBER_LOOPS', help="""Start capturing right away, 
-        with the given duration in loops. """)
-    
-    # Parse the command line arguments
-    cml_args = arg_parser.parse_args()
-    
     
     # If the duration of capture was given, capture right away for a specified time in hours
     if cml_args.duration:
@@ -113,13 +172,11 @@ if __name__ == "__main__":
         # Convert the given number of loops to floating point hours by dividing loops by loops per hour
         duration = (float(cml_args.loops))/7.2
 
-
-
     # If a duration was given in loops or hours, run for the desired length of time
     if (cml_args.duration or cml_args.loops):
         
         
-        stored_path = makeDirectory(station_code, channel, os.path.join(path, raw) + "/")
+        stored_path = makeDirectory(station_code, channel, os.path.join(DEFAULT_PATH, raw))
         
         # ads1256.run returns 0 when its done recording
         running = ads1256.run(duration, mode, station_code, channel, latitude, longitude, elevation, instrument_string, stored_path)
@@ -127,50 +184,59 @@ if __name__ == "__main__":
         # Compress all files saved
         source = stored_path.replace("//","/")[:-1]
         
-        # Takes the 
-        archiveDir(source, os.listdir(source), os.path.join(path, zipped) , os.path.split(source)[1])
-        
+        # If the ADC had a faulty exit, end the program
         if running == 1:
             print("\nProgram terminated!\n")
             sys.exit()
+        # If it exited safely, zip the data
+        else:
+            # Zips the files and moves the =m to the archiving directory
+            archiveDir(source, os.listdir(source), os.path.join(DEFAULT_PATH, zipped) , os.path.split(source)[1])
     
     # If no duration given, it is assumed to wait for night and run all night.
     else:
         while(1):
             
+            # Get a datetime object of exactly when sunset is at -5.5 deg and how long to run for until sunrise in seconds
             start_time, duration = captureDuration(latitude,longitude,elevation)
+            
             # Convert duration to hours
             duration/=3600
             
-            # Wait to start capturing
+            # If start_time holds a datetime object as opposed to being True
             if start_time != True:
                 
-                # Calculate how many seconds to wait until capture starts, and with for that time
+                # Get a current datetime object
                 time_now = datetime.datetime.utcnow()
+                # Calculate the difference in time between now and when the recording should start
                 waiting_time = start_time - time_now
                 
+                # Let the user know when it will start recording in UTC
                 print("Recording will start on:", start_time, "UTC")
                 
-                # Wait until sunset
+                # Wait until sunset using the wait time in seconds
                 time.sleep(int(waiting_time.total_seconds()))
                 
+            # Creates the directory to store the data from this current run and returns the file path used to 
+            # store the data in c, uses two slashes instead of one "/" -> "//"
+            stored_path = makeDirectory(station_code, channel, os.path.join(DEFAULT_PATH, raw))
     
-            path = makeDirectory(station_code, channel, os.path.join(path, raw) + "/")
-    
-            # ads1256.run returns 0 when its done recording
+            # ads1256.run returns 0 when its done recording, or 1 for a forced exit, automatically saves data
+            # to stored_path
             running = ads1256.run(duration, mode, station_code, channel, latitude, longitude, elevation, instrument_string, stored_path)
             
-            # Compress all files saved
+            # Remove the extra slash used at the end of the path (needs to be there for c code), and convert 
+            # back to single slashes. The last slash is ignored by taking all but the last element
             source = stored_path.replace("//","/")[:-1]
-        
-            # Takes the 
-            archiveDir(source, os.listdir(source), os.path.join(path, zipped) , os.path.split(source)[1])
             
+            # If the ADC had a faulty exit, end the program
             if running == 1:
                 print("\nProgram terminated with ctrl c!\n")
                 sys.exit()
+            # If it exited safely, zip the data
             else:
-                continue
+                # Zips the files and moves the =m to the archiving directory
+                archiveDir(source, os.listdir(source), os.path.join(DEFAULT_PATH, zipped) , os.path.split(source)[1])
         
         
         
