ebb269012821276fd7cd83d8c42d1bd49bd58d35
Denis Vida <denis.vida@gmail.com>
2018-07-23 13:57:57 +0000

    fixed installation and config generation

--- python3_radiometer_installation.sh
+++ python3_radiometer_installation.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
 sudo apt-get install -y python3-opengl
 sudo apt-get install -y python3-pyqt5
 sudo apt-get install -y python3-wxgtk3.0*
 sudo apt-get install -y python3-pyside
 sudo apt-get install -y liblapack*
 sudo apt-get install -y python3-matplotlib
-sudo apt-get install libatlas-base-dev
+sudo apt-get install -y libatlas-base-dev
 sudo apt install -y libffi-dev libffi6
 sudo apt install -y python3-cairo 
+sudo apt-get install -y python3-pip
 sudo pip3 install cairocffi cffi
 sudo pip3 install numpy --upgrade
 sudo pip3 install scipy
 sudo pip3 install vispy
 sudo pip3 install cryptography
 sudo pip3 install paramiko
+sudo pip3 install pyephem
